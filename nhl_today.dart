08e92fa41fa37361c337cd9b13f6ef457e0bd805
Blake Lapum <lapumb@mail.gvsu.edu>
2020-09-02 22:19:03 -0400

    print raw json response

--- nhl_today.dart
+++ nhl_today.dart
@@ -1,5 +1,16 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-void main() {
-  
+void main() async {
+  var client = http.Client(); 
+  var url = 'https://statsapi.web.nhl.com/api/v1/schedule';
+
+  // Await the http get response, then decode the json-formatted response.
+  var response = await client.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    print('Raw response: \n\n$jsonResponse');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
 }
